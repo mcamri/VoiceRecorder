c732f218751108c523ffb430ce3a1895315fb955
Amri <chozinul@gmail.com>
2019-05-05 13:29:42 +0800

    Add navigation.

---
+++ app/navigation/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import { createAppContainer, createStackNavigator } from "react-navigation"
+
+import Home from '../page/Home'
+
+const StackNavigator = createStackNavigator({ Home });
+
+const AppContainer = createAppContainer(StackNavigator);
+
+export default class App extends Component {
+  render() {
+    return (
+       <AppContainer />
+    );
+  }
+}
+
+
---
+++ app/page/Home.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+import {Platform, Text, View} from 'react-native'
+
+import styles from './Home.style'
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class Home extends Component {
+
+  static navigationOptions = {
+    title: 'Voice Recorder',
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+
