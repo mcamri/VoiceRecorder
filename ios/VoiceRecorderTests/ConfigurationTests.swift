bd4c997c5a6e3109063ae63d1f767957ba84ddf4
Amri <chozinul@gmail.com>
2019-05-07 08:17:19 +0800

    Added unit test for config. Support Xcode 10.2.1

---
+++ ios/VoiceRecorderTests/ConfigurationTests.swift
@@ -0,0 +1,9 @@
+//
+//  ConfigurationTests.swift
+//  VoiceRecorderTests
+//
+//  Created by Muchamad Chozinul Amri on 7/5/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
